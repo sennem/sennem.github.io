64b01180eb14fd0b1ace492d7deb4c6ea6145d7e
sennem <marksenne000@gmail.com>
2018-04-23 18:27:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function helloworld(name)
 {
-  alert("Hello " + name + '!');
-        
+  alert("Hello " + name + '!');     
 }
+
+var fname = document.GetElementById(firstname).value;
+var lname = document.GetElementById(lastname).value;
+var stuid = document.GetElementById(studentid).value;
+var grade = document.GetElementById(gradenum).value;
+
+var alldata = '<div>Firstname: ' + fname + '</div>' + '<br>' + '<div>Lastname: ' + lname + '</div>' + '<br>' + '<div>StudentID: ' + stuid + '</div>' + '<br>' + '<div>Grade: ' + grade + '</div>' + '<br>' + '<div>Drive: ' + drive + '</div>';
+document.GetElementById(allstudentinfo).innerHTML=alldata;
