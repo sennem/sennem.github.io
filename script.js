c794be29b83fa3f39ee4f3994478822aca7ce074
sennem <marksenne000@gmail.com>
2018-04-24 08:59:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,6 @@ function processinfo()
   //var grade = document.unserinfo.Grade.value;
   //var drive = document.userinfo.ddrive.value;
   var alldata = '<div>First Name: ' + fname + '</div>'+ '<div>Last Name: ' +lname +'<div>' + '<div>ID: ' + stuid + '<div>';
-  document.getElementById(allstudentinfo).innerHTML+=alldata;
-  //document.GetElementById(allstudentinfo).innerHTML="testing plz work";
+  //document.getElementById(allstudentinfo).innerHTML+=alldata;
+  document.getElementById(allstudentinfo).innerHTML="testing plz work";
 }
