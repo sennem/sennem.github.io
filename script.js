6d7d387bbfb3a4dc7e56f7d01b2c3508489386cb
sennem <marksenne000@gmail.com>
2018-04-24 08:09:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ function processinfo()
   var fname = document.userinfo.firstname.value;
   var lname = document.userinfo.lastname.value;
   var stuid = document.userinfo.studentid.value;
-  var grade = document.GetElementById(gradenum).value;
-  var drive = document.GetElementById(driveyn).value;
+  var grade = document.unserinfo.Grade.value;
+  var drive = document.userinfo.ddrive.value;
   var alldata = '<div>First Name: ' + fname + '</div>'+ '<div>Last Name: ' +lname +'<div>' + '<div>ID: ' + stuid + '<div>';
-  //document.GetElementById(allstudentinfo).innerHTML=alldata;
-  document.GetElementById(allstudentinfo).innerHTML="testing plz work";
+  document.GetElementById(allstudentinfo).innerHTML+=alldata;
+  //document.GetElementById(allstudentinfo).innerHTML="testing plz work";
 }
