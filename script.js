e4a1d492c1367b5158f0f4bb8721394b728300f5
sennem <marksenne000@gmail.com>
2018-04-24 09:48:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-function helloworld(name)
-{
-  alert("Hello " + name + '!');     
-}
 
-function processinfo()
-{
-  var fname = document.userinfo.firstname.value;
-  var lname = document.userinfo.lastname.value;
-  var stuid = document.userinfo.studentid.value;
-  //var grade = document.unserinfo.Grade.value;
-  //var drive = document.userinfo.ddrive.value;
-  var alldata = '<div>First Name: ' + fname + '</div>'+ '<div>Last Name: ' +lname +'<div>' + '<div>ID: ' + stuid + '<div>';
-  //document.getElementById(allstudentinfo).innerHTML+=alldata;
-  document.getElementById(allstudentinfo).innerHTML="testing plz work";
-}
