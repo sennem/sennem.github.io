fc54456534ffdf4b7f4c2326cff2437972008cac
sennem <marksenne000@gmail.com>
2018-04-24 07:20:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ function processinfo()
   var fname = document.userinfo.firstname.value;
   var lname = document.userinfo.lastname.value;
   var stuid = document.userinfo.studentid.value;
-  var grade = document.userinfo.gradenum.value;
-  var drive = document.userinfo.drive.value;
+  var grade = document.GetElementById(gradenum).value;
+  var drive = document.GetElementById(driveyn).value;
   var alldata = '<div>First Name: ' + fname + '</div>'+ '<div>Last Name: ' +lname +'<div>' + '<div>ID: ' + stuid + '<div>';
   document.GetElementById(allstudentinfo).innerHTML=alldata;
   //document.GetElementById(allstudentinfo).innerHTML="testing plz work";
