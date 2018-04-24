80548175c5c83fbc00ce948deff4a1996e46fa29
sennem <marksenne000@gmail.com>
2018-04-24 07:08:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function helloworld(name)
   alert("Hello " + name + '!');     
 }
 
-funtion processinfo()
+function processinfo()
 {
   var fname = document.userinfo.firstname.value;
   var lname = document.userinfo.lastname.value;
@@ -11,6 +11,6 @@ funtion processinfo()
   var grade = document.userinfo.gradenum.value;
   var drive = document.userinfo.drive.value;
   var alldata = '<div>First Name: ' + fname + '</div>'+ '<div>Last Name: ' +lname +'<div>' + '<div>ID: ' + stuid + '<div>';
-  //document.GetElementById(allstudentinfo).innerHTML=alldata;
-  document.GetElementById(allstudentinfo).innerHTML="testing plz work";
+  document.GetElementById(allstudentinfo).innerHTML=alldata;
+  //document.GetElementById(allstudentinfo).innerHTML="testing plz work";
 }
