dde360610915981669261c443784e05a41bfef25
sennem <marksenne000@gmail.com>
2018-04-24 07:02:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,5 +11,6 @@ funtion processinfo()
   var grade = document.userinfo.gradenum.value;
   var drive = document.userinfo.drive.value;
   var alldata = '<div>First Name: ' + fname + '</div>'+ '<div>Last Name: ' +lname +'<div>' + '<div>ID: ' + stuid + '<div>';
-  document.GetElementById(allstudentinfo).innerHTML=alldata;
+  //document.GetElementById(allstudentinfo).innerHTML=alldata;
+  document.GetElementById(allstudentinfo).innerHTML="testing plz work";
 }
